9967a5d8f2aecd03ebf4dd0c3f128d74f70acd6e
ptejalaxioned <tejalp@axioned.com>
2024-10-08 18:31:40 +0530

    function of custom texonomy called category commit

--- wp-content/themes/sixteen-clothing/functions.php
+++ wp-content/themes/sixteen-clothing/functions.php
@@ -94,3 +94,32 @@ function create_custom_post_type_products() {
   register_post_type( 'products', $args );
 }
 add_action( 'init', 'create_custom_post_type_products' );
+
+//Custom Texonomy 
+function create_custom_taxonomy_category() {
+  $labels = array(
+      'name'              => __( 'Categories' ),
+      'singular_name'     => __( 'Category' ),
+      'search_items'      => __( 'Search Categories' ),
+      'all_items'         => __( 'All Categories' ),
+      'parent_item'       => __( 'Parent Category' ),
+      'parent_item_colon' => __( 'Parent Category:' ),
+      'edit_item'         => __( 'Edit Category' ),
+      'update_item'       => __( 'Update Category' ),
+      'add_new_item'      => __( 'Add New Category' ),
+      'new_item_name'     => __( 'New Category Name' ),
+      'menu_name'         => __( 'Categories' ),
+  );
+
+  $args = array(
+      'labels'            => $labels,
+      'hierarchical'      => true, // Set to true for hierarchical taxonomy (like categories)
+      'public'            => true,
+      'show_admin_column' => true,
+      'show_in_rest'      => true, // Enables Gutenberg editor
+      'rewrite'           => array( 'slug' => 'category' ),
+  );
+
+  register_taxonomy( 'category', array( 'products' ), $args );
+}
+add_action( 'init', 'create_custom_taxonomy_category' );
