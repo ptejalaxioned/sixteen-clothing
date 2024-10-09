f5a79ab57f0732cd279fa5b8e3f0eee9ca749e50
ptejalaxioned <tejalp@axioned.com>
2024-10-09 14:05:29 +0530

    footer section  in footer.php

--- wp-content/themes/sixteen-clothing/footer.php
+++ wp-content/themes/sixteen-clothing/footer.php
@@ -1,11 +1,26 @@
+<?php
+$copyright_text=get_field('copyright_text','options');
+$reference_link=get_field('reference_link','options');
+?>
 </main>
 <!--main section end-->
+<?php
+if($copyright_text || $reference_link){
+  ?>
 <!--footer section start-->
 <footer>
-  <div class="wrapper">
-  </div>
+<div class="wrapper flex justify-center py-11">
+        <?php if($copyright_text){?>
+          <span
+            class="first-caps flex justify-end text-xs text-gray-600 uppercase"
+            ><?php echo $copyright_text ?>
+            <?php echo linkAttributes($copyright_text , 'text-orangered'); ?>
+          </span>
+            <?php } ?>
+        </div>
 </footer>
 <!--footer section end-->
+<?php } ?>
 </div>
 <!--container end-->
 <?php wp_footer(); ?>
